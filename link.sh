36bb74ee6fb84ed81bf444449c37fea4d6bf5910
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-03-10 13:19:10 +0000

    Do not force link

--- link.sh
+++ link.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-ln -sf "$(realpath i3/config)" "$HOME/.config/i3/config"
+# i3
+ln -s "$(realpath i3/config)" "$HOME/.config/i3/config"
 
+# zprezto
 setopt EXTENDED_GLOB
 for rcfile in zprezto/runcoms/^README.md(.N); do
   ln -s "$(realpath $rcfile)" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
