a875af1c458eb5b93a204deaeec14861dc488d11
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-03-10 13:17:54 +0000

    Add zprezto

--- link.sh
+++ link.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 ln -sf "$(realpath i3/config)" "$HOME/.config/i3/config"
+
+setopt EXTENDED_GLOB
+for rcfile in zprezto/runcoms/^README.md(.N); do
+  ln -s "$(realpath $rcfile)" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
