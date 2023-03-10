8bac31e20505c8104cc0fb3383a79b78207c3b5a
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-03-10 13:28:31 +0000

    Link i3 and nvim dirs

--- link.sh
+++ link.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
 # i3
-ln -s "$(realpath i3/config)" "$HOME/.config/i3/config"
+ln -s "$(realpath i3)" -t ~/.config
 
 # zprezto
 setopt EXTENDED_GLOB
 for rcfile in zprezto/runcoms/^README.md(.N); do
   ln -s "$(realpath $rcfile)" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+# nvim
+ln -s "$(realpath nvim)" -t ~/.config
