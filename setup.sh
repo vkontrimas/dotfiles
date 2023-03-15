af577bfa65a91779745ed2c3558a1106aeb55578
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-03-15 12:40:49 +0000

    i3: launch rofi exit

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ mkdir -p "$I3_WALLPAPER_DIR"
 ln -s "$(realpath i3/wallpaper.jpg)" "$I3_WALLPAPER_DIR/.i3_wallpaper.jpg"
 
 ln -s "$(realpath i3/lock.sh)" "$HOME/.lock.sh"
+ln -s "$(realpath i3/rofi_exit.sh)" "$HOME/.rofi_exit.sh"
 
 I3_CONFIG_DIR="$HOME/.config/i3"
 mkdir -p "$I3_CONFIG_DIR"
