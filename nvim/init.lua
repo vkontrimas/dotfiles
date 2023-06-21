ec1f8486c6dac62343065ee9042f42c811c4ad30
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-06-21 14:55:28 +0100

    nvim: switch to lua

---
+++ nvim/init.lua
@@ -0,0 +1,35 @@
+-- Plugin Manager Setup
+require('plugins')
+
+-- General Settings
+vim.opt.termguicolors = true
+
+vim.g.ayucolor = 'dark'
+vim.cmd [[colorscheme ayu]]
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.foldcolumn = '2'
+
+vim.opt.colorcolumn = '80,120'
+
+-- Key Mappings
+vim.api.nvim_set_keymap('n', '<leader>h', '<C-W>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-W>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-W>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<C-W>l', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>b', ':b#<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':Rg<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>t', ':BLines<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>g', ':Buffers<CR>', { noremap = true })
+
+-- Plugin-Specific Settings
+-- vim.g.clang_format#auto_format = 0
+-- vim.api.nvim_set_keymap('n', '<Leader>C', ':ClangFormatAutoToggle<CR>', { noremap = true })
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'ayu-theme/ayu-vim'
+  use 'tpope/vim-surround'
+  use 'junegunn/fzf.vim'
+  use 'junegunn/fzf'
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+end)
