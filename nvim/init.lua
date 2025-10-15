fddd8b26df48a8d9e4d8a6cc208cf5eb10862580
Vytautas <vkon@vkon.io>
2025-10-15 10:18:33 +0100

    feat: add coq snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -187,7 +187,7 @@ require("lazy").setup({
         { "ms-jpq/coq_nvim", branch = "coq" },
 
         -- 9000+ Snippets
-        -- { "ms-jpq/coq.artifacts", branch = "artifacts" },
+        { "ms-jpq/coq.artifacts", branch = "artifacts" },
 
         -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
         -- Need to **configure separately**
