751faf4bb876bee56b17134461a3311f88af01ab
Vytautas Kontrimas <vytautas.kontrimas@rocketscience.gg>
2026-07-31 15:02:19 +0100

    feat(nvim): add <C>-HJKL for navigating splits

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,6 +54,12 @@ vim.g.editorconfig = false -- fuck off
 vim.keymap.set("n", "gb", ":e #<CR>")
 vim.keymap.set("n", "gB", ":vsp #<CR>")
 
+-- Navigate splits with Ctrl+HJKL
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+
 --------------------------------------------------------------------------------
 -- SETTINGS OVER SETTINGS OVER SETTINGS OVER                                  --
 --------------------------------------------------------------------------------
